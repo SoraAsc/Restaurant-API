0b2f43181c66e612996914303a8c7d164ec55057
Marcos Vinícios Dias Ferreira <57465032+SoraAsc@users.noreply.github.com>
2022-02-15 19:13:43 -0300

    Adição das principais depedências.

---
+++ src/test/java/br/com/diasmarcos/sistemaloja/SistemalojaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diasmarcos.sistemaloja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemalojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
