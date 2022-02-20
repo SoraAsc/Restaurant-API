1cdabd62be780baad1de33a0d663be33caee8a00
Marcos Vinícios Dias Ferreira <57465032+SoraAsc@users.noreply.github.com>
2022-02-20 14:21:07 -0300

    Adição de unidades de medidas adicionais.

--- src/main/java/br/com/diasmarcos/sistemaloja/enums/MeasurementUnits.java
+++ src/main/java/br/com/diasmarcos/sistemaloja/enums/MeasurementUnits.java
@@ -6,7 +6,14 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum MeasurementUnits {
-    g(""), Kg("");
+    //Massa
+    mg("Miligrama"), g("Grama"), Kg("Quilograma"),
+    //Volume
+    tsp("Colher de Chá"), c("Xícara"), gallon("Galão"),
+    ml("Mililitro"), l("Litro"),
+    //Comprimento
+    cm("Centímetro"), m("Metro"), inch("Polegada");
+
 
     private final String description;
 }
